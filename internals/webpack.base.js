63390421db05578b99201cb343aeda97c8f358b2
sujithnath <sujithgnth@gmail.com>
2020-06-04 11:07:59 +0530

    chnages in webconfgig

---
+++ internals/webpack.base.js
@@ -0,0 +1,46 @@
+/**
+ * COMMON WEBPACK CONFIGURATION
+ */
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+      {
+        test: /\.(.js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(eot|otf|ttf|woff|woff2)$/,
+        use: "file-loader",
+      },
+    ],
+  },
+};
